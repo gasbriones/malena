0b376b75deb7219a27d59aa7311c3a92d4be3de4
Gaston Briones <gbriones@cmd.com.ar>
2016-06-10 00:30:32 -0300

    maqueta

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
-var babel = require('gulp-babel')
-var browserify = require('browserify')
-var concat = require('gulp-concat')
-var connect = require('gulp-connect')
 var gulp = require('gulp')
+var connect = require('gulp-connect')
 var livereload = require('gulp-livereload')
-var rename = require('gulp-rename')
 var sass = require('gulp-sass')
-var uglify = require('gulp-uglify')
-var source = require('vinyl-source-stream')
-var buffer = require('vinyl-buffer')
-var watchify = require('watchify')
 var haml = require('gulp-haml');
-var fileinclude = require('gulp-file-include');
+var prettify = require('gulp-prettify');
 
 gulp.task('server', function () {
     connect.server({
@@ -22,20 +14,11 @@ gulp.task('server', function () {
     })
 })
 
-gulp.task('fileinclude', function() {
-    gulp.src(['index.html'])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '/app/'
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-
 gulp.task('haml', function () {
     gulp.src('app/**/*.haml')
         .pipe(haml({
             compiler: 'visionmedia',
+            format: 'html'
         }))
         .pipe(gulp.dest('dist'))
         .pipe(livereload())
@@ -43,15 +26,22 @@ gulp.task('haml', function () {
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/css/'))
         .pipe(livereload())
 })
+
+gulp.task('prettify', function() {
+    gulp.src('dist/*.html')
+        .pipe(prettify({indent_size: 2}))
+        .pipe(gulp.dest('dist'))
+});
 
 gulp.task('watch', function() {
     livereload.listen()
     gulp.watch('app/scss/*.scss', ['sass']);
     gulp.watch('app/**/*.haml', ['haml']);
+    gulp.watch('dist/*.html', ['prettify']);
 })
 
-gulp.task('dev', ['watch', 'sass', 'server','haml'])
+gulp.task('dev', ['watch', 'sass', 'server','haml','prettify'])
