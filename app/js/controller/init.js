87b9d8e4ba99c09a32a36ecb020726194a7ada0e
Juan Farias <dientuki@gmail.com>
2016-07-11 08:11:50 -0300

    fix init

--- app/js/controller/init.js
+++ app/js/controller/init.js
@@ -1,6 +1,3 @@
-import ImageGallery from "../../../dist/build/components/carousel-home.js"
-import Slider from "../../../dist/build/components/slider-home.js"
-
-import Articles from "../../../dist/build/components/publications-home.js"
-
-
+//import ImageGallery from "../../../dist/build/components/carousel-home.js"
+import Slider from "../../../dist/build/components/slider-home.js";
+import Articles from "../../../dist/build/components/publications-home.js";
